a5b1b86572a9c8c404122aab9d84ebd30969a012
Yana <YanaLSoltan@gmail.com>
2018-02-21 23:33:00 +0300

    task 4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+//module.exports =
+function solveEquation(equation) {
+
+    var a= (equation.substring(0, equation.indexOf('*'))).replace(/\s+/g, '');
+    var b= (equation.substring(equation.indexOf('^2')+2, equation.indexOf('*', equation.indexOf('^2'))-1)).replace(/\s+/g, '');
+    var c= (equation.substring(equation.indexOf('x', equation.indexOf('^2'))+1)).replace(/\s+/g, '');
+    var D=0;
+    var x1=null;
+    var x2=null;
+
+
+
+
 }
+
+
+var equation = '-417 * x^2 + 806708601 * x + 3174554007677316';
+
+console.log( solveEquation(equation));
