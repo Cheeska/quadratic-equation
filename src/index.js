107269a54bf49ee4a3ef754b42c4efbe3e46e957
Yana <YanaLSoltan@gmail.com>
2018-02-22 15:54:06 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = function solveEquation(equation) {
+module.exports =  function solveEquation(equation) {
 
     var a= (equation.substring(0, equation.indexOf('*'))).replace(/\s+/g, '');
     var b= (equation.substring(equation.indexOf('^2')+2, equation.indexOf('*', equation.indexOf('^2'))-1)).replace(/\s+/g, '');
@@ -7,22 +7,28 @@ module.exports = function solveEquation(equation) {
     var x2= null;
     var arr= new Array();
 
-   var D = b*b - 4*a*c;
+   var D = Math.pow(b,2) - 4*a*c;
 
    if(D>0){
-       var x1 = (-b + Math.sqrt(D)) / (2*a);
-       var x2 = (-b - Math.sqrt(D)) / (2*a);
+       var x1 = Math.round((-b + Math.sqrt(D)) / (2*a));
+       var x2 = Math.round((-b - Math.sqrt(D)) / (2*a));
    }
    else{
-       var x1 = (-b + Math.sqrt(D)) / 2*a;
+       var x1 =  Math.round((-b + Math.sqrt(D)) / (2*a));
    }
 
-   arr.push(Math.round(x1));
-   arr.push(Math.round(x2));
+    if (x1>x2){
+       arr.push(x2);
+       arr.push(x1);
 
-   return arr.sort(comparator());
+    }else{
+        arr.push(x1);
+        arr.push(x2);
+    }
+    return arr;
 }
 
 function comparator() {
     (left, right) => left - right;
 }
+
